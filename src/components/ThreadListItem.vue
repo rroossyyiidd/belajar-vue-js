a3c0a400ecea28c8e71a1c41a4da8ea1f652355e
Rsyd <rroossyyiidd@gmail.com>
2019-01-08 14:56:34 +0700

    Components, Props and Computed Properties

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="#">{{thread.title}}</a>
+      </p>
+      <p class="text-faded text-xsmall">
+        By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        {{repliesCount}} replies
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data'
+
+  export default {
+    name: 'ThreadListItem',
+    props: {
+      thread: {
+        required: true,
+        type: Object
+      }
+    },
+    computed: {
+      // fungsi untuk menghitung balasan
+      repliesCount () {
+        return Object.keys(this.thread.posts).length - 1
+      },
+      user () {
+        return sourceData.users[this.thread.userId]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
