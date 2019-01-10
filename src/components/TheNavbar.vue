3fe949ab0088c44f75703ee4a7ba3a32d8693fe5
Rsyd <rroossyyiidd@gmail.com>
2019-01-10 11:17:29 +0700

    mapGetters vuex

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -12,10 +12,10 @@
 
     <li class="navbar-user">
       <a href="#">
-        <img class="avatar-small" src="https://pbs.twimg.com/profile_images/881260299420041217/GMVGlDea_400x400.jpg"
+        <img class="avatar-small" :src="user.avatar"
              alt="">
         <span>
-                Alex Kyriakidis
+                {{user.name}}
                 <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
             </span>
       </a>
@@ -35,8 +35,15 @@
 </template>
 
 <script>
+  import {mapGetters} from 'vuex'
+
   export default {
-    name: 'TheNavbar'
+    name: 'TheNavbar',
+    computed: {
+      ...mapGetters({
+        'user': 'authUser'
+      })
+    }
   }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,15 @@ import sourceData from '@/data'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: sourceData,
+  state: {
+    ...sourceData,
+    authId: 'jVa6Go6Nl1Urkag1R2p9CHTf4ny1'
+  },
+  getters: {
+    authUser (state) {
+      return state.users[state.authId]
+    }
+  },
   actions: {
     createPost (context, post) {
       const postId = 'greatPost' + Math.random()
